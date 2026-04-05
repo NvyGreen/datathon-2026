88f93591a9ffe13083282a835e837a7f792ae043
Pratham Hebbar <prathamhebbar2021@gmail.com>
2026-04-04 22:04:26 -0700

    consumer files

--- script.py
+++ script.py
@@ -32,24 +32,43 @@ property_data_columns = ['NumberOfBedrooms', 'GarageParkingNumberOfCars', 'Total
 consumer_data = pd.read_csv('consumer_clean.csv')
 property_data = pd.read_csv('PropertyAssessmentData_cleaned.csv')
 
-consumer = consumer_data[consumer_data_columns].copy()
-property = property_data[property_data_columns].copy()
+consumer_data['Address'] = consumer_data['Address'].str.strip().str.lower()
+property_data['PropertyAddress'] = property_data['PropertyAddress'].str.strip().str.lower()
 
-# Rename property columns to match consumer columns
-property.columns = consumer_data_columns
+merged_data = pd.merge(consumer_data, property_data, left_on='Address', right_on='PropertyAddress', how='inner')
+
+X = merged_data[consumer_data]
+y = merged_data[property_data]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
 from sklearn.preprocessing import MinMaxScaler
 
 scaler = MinMaxScaler()
 
-property_scaled = scaler.fit_transform(property)
-consumer_scaled = scaler.fit_transform(consumer)
+property_scaled = scaler.fit_transform(X_train)
+consumer_scaled = scaler.fit_transform(X_test)
 
 nn = NearestNeighbors(n_neighbors=3, metric='cosine')
 nn.fit(property_scaled)
 
 distances, indices = nn.kneighbors(consumer_scaled)
 
-top_k_houses = property.iloc[indices[0]].copy()
-top_k_houses["Similarity"] = 1 - distances[0]
-print(top_k_houses)
+# top_k_houses = property.iloc[indices[0]].copy()
+# top_k_houses["Similarity"] = 1 - distances[0]
+# print(top_k_houses)
+
+
+
+knn = KNeighborsClassifier(n_neighbors=3)
+knn.fit(X_train, y_train)
+y_pred = knn.predict(X_test)
+
+accuracy_percentage = knn.score(X_test, y_test)
+print(accuracy_percentage)
+
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+
+cr = classification_report(y_test, y_pred)
+print(cr)
